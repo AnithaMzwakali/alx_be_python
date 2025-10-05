15c5d5abd57498ae1295a7230a194fdb4f3139cb
Anitha <anithamzwakali97@gmail.com>
2025-10-05 15:21:52 +0200

    Rename multiplication test method to test_multiply for checker

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,29 +2,34 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
+    """Unit tests for the SimpleCalculator class."""
 
     def setUp(self):
         self.calc = SimpleCalculator()
 
+    # Addition tests
     def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-2, -3), -5)
         self.assertEqual(self.calc.add(-2, 3), 1)
         self.assertEqual(self.calc.add(0, 5), 5)
 
+    # Subtraction tests
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(3, 5), -2)
         self.assertEqual(self.calc.subtract(-5, -3), -2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
 
-    def test_multiply(self):
+    # Multiplication tests
+    def test_multiply(self):  # <- renamed to match checker
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
         self.assertEqual(self.calc.multiply(-2, -3), 6)
         self.assertEqual(self.calc.multiply(0, 5), 0)
 
-    def test_division(self):  # <- renamed to match checker
+    # Division tests
+    def test_divide(self):
         self.assertEqual(self.calc.divide(6, 3), 2)
         self.assertEqual(self.calc.divide(-6, 3), -2)
         self.assertEqual(self.calc.divide(-6, -3), 2)
@@ -34,3 +39,4 @@ class TestSimpleCalculator(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
