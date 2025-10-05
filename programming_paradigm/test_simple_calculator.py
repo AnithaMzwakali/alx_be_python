d742d6c2e75f035a3d1511eee74c95e1b4dab2a9
Anitha <anithamzwakali97@gmail.com>
2025-10-05 15:28:16 +0200

    Fix test method names: add aliases for multiply and divide for checker

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,35 +6,42 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    # Addition tests
+    # Addition
     def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-2, -3), -5)
         self.assertEqual(self.calc.add(-2, 3), 1)
         self.assertEqual(self.calc.add(0, 5), 5)
 
-    # Subtraction tests
+    # Subtraction
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(3, 5), -2)
         self.assertEqual(self.calc.subtract(-5, -3), -2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
 
-    # Multiplication tests
-    def test_multiply(self):
+    # Multiplication
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
         self.assertEqual(self.calc.multiply(-2, -3), 6)
         self.assertEqual(self.calc.multiply(0, 5), 0)
 
-    # Division tests
-    def test_divide(self):
+    # Alias for checker expecting test_multiply
+    def test_multiply(self):
+        self.test_multiplication()
+
+    # Division
+    def test_division(self):
         self.assertEqual(self.calc.divide(6, 3), 2)
         self.assertEqual(self.calc.divide(-6, 3), -2)
         self.assertEqual(self.calc.divide(-6, -3), 2)
         self.assertIsNone(self.calc.divide(5, 0))
         self.assertIsNone(self.calc.divide(0, 0))
         self.assertEqual(self.calc.divide(0, 5), 0)
+
+    def test_divide(self):
+        self.test_division()
 
 if __name__ == "__main__":
     unittest.main()
